92d46ed8091965bee97c83ff3decc518641c15fd
Angellyc Almeida Bassi <162837706+angellycbassi@users.noreply.github.com>
2024-12-11 11:06:23 -0300

    Atuaçl

--- BD - GJI.sql
+++ BD - GJI.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE trabalho;
-USE  Sakila;
+-- CREATE DATABASE trabalho;
+USE  trabalho;
 
 CREATE TABLE turma (
     IDturma INT PRIMARY KEY,
@@ -15,19 +15,35 @@ CREATE TABLE participante (
     IDparticipante INT PRIMARY KEY,
     IDturma INT,
     IDcategoria INT,
-    matricula INT,
     nome VARCHAR(100) NOT NULL,
+    cpf INT,
     data_nascimento DATE,
     email VARCHAR(100),
-        FOREIGN KEY (IDturma) REFERENCES turma (IDturma),
-		FOREIGN KEY (IDcategoria) REFERENCES categoria (IDcategoria)
+    esporte VARCHAR(100),
+        FOREIGN KEY (IDturma) REFERENCES turma(IDturma),
+		FOREIGN KEY (IDcategoria) REFERENCES categoria(IDcategoria)
 );
 
-CREATE TABLE jogos (
-    IDjogos INT PRIMARY KEY,
+CREATE TABLE times (
+    IDtime INT PRIMARY KEY,
+    turma INT,
+    FOREIGN KEY (turma) REFERENCES times(IDturma)
+);
+
+CREATE TABLE jogo (
+    IDjogo INT PRIMARY KEY,
+    IDtime1 INT NOT NULL,
+    IDtime2 INT NOT NULL,
+    horario DATETIME NOT NULL,
+    placar_time1 INT DEFAULT 0,
+    placar_time2 INT DEFAULT 0,
+    FOREIGN KEY (IDtime1) REFERENCES times(IDtime),
+    FOREIGN KEY (IDtime2) REFERENCES times(IDtime)
 );
 
 CREATE TABLE classificacao (
     IDclassificacao INT PRIMARY KEY,
+    IDtime INT NOT NULL,
+    pontos INT DEFAULT 0,
+    FOREIGN KEY (IDtime) REFERENCES times(IDtime)
 );
-
